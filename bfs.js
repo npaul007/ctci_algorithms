376e45c317dc8ac16928d2429889a385995d6228
Nathanael Paulemon <npaulemon@gmail.com>
2020-07-13 14:59:40 -0400

    Update bfs.js
    update bfs 3

--- bfs.js
+++ bfs.js
@@ -2,6 +2,10 @@
 function Graph () {
     this.nodes = {};
     
+    this.size = function () {
+        return Object.keys(this.nodes).length;
+    }
+
     this.addNode = function (node) {
         this.nodes[node.value] = node;
     }
@@ -9,15 +13,31 @@ function Graph () {
     this.removeNode = function (value) {
         delete this.nodes[value];
     }
+
+    this.get = function (value) {
+        return this.nodes[value];
+    }
 }
 
 function Node (value,edges) {
     this.value = value;
-    this.edges = edges
+    this.edges = edges;
+    this.traversed = false;
 }
 
-function BFS(node) {
+function BFS(nodeValue,graph) {
+    let traversedNodes = [];
 
+    let queue = [];
+    queue.enqueue = queue.unshift;
+    queue.dequeue = queue.pop;
+
+    let curNode = graph.get(nodeValue);
+
+    queue.unshift(curNode.value);
+    traversedNodes.push(curNode.value);
+
+    return traversedNodes.join('\n');
 }
 
 let graph = new Graph();
@@ -31,5 +51,5 @@ graph.addNode( new Node('E',[]) );
 graph.addNode( new Node('F',[]) );
 graph.addNode( new Node('H',['E']) );
 
-console.log(graph);
+console.log();
 
